69af63368e3308b4997a8285192c95574a280d9e
jam <jonghwa.hong@multicampus.com>
2025-01-27 05:36:13 +0000

    .

--- deep-chat-v2/react/vite.config.ts
+++ deep-chat-v2/react/vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    host: '0.0.0.0',
+    allowedHosts: true,
+  },
+  preview:{
+    host: '0.0.0.0',
+    allowedHosts: true,
+  },
 })
+
+// ...
+// import dotenv from 'dotenv'
+// dotenv.config()
+// const ALLOWED_HOSTS = process.env['ALLOWED_HOSTS']?.split(',')
+
